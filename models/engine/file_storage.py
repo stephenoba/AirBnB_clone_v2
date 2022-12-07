289d006fb3ac31272a65b0ac08db1300d1eff5a6
Stephen Oba <obastepheno@gmail.com>
2022-12-07 17:47:08 +0100

    Add method .delete(self, obj=None) and Update .all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,13 +8,22 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        objs = FileStorage.__objects
+        if cls:
+            filtered_objs = {}
+
+            for k, v in objs.items():
+                if isinstance(v, cls):
+                    filtered_objs[k] = v
+            return filtered_objs
+        return objs
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        _key = self.build_obj_key(obj)
+        self.all().update({_key: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -27,6 +36,29 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
+        try:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    cls = self.get_class(val['__class__'])
+                    self.all()[key] = cls(**val)
+        except (FileNotFoundError, KeyError):
+            pass
+
+    def delete(self, obj=None):
+        """delete an object from __objects"""
+        if obj:
+            key = self.build_obj_key(obj)
+            self.all().pop(key)
+            del obj
+            self.save()
+
+    # HELPER METHODS
+    # =============================================
+
+    def get_class(self, cls_name):
+        """get corresponding class from string"""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -36,15 +68,14 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
+                'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                'State': State, 'City': City, 'Amenity': Amenity,
+                'Review': Review
+        }
+
+        return classes[cls_name]
+
+    def build_obj_key(self, obj):
+        """Build or retrieve obj key in __objects"""
+        _key = obj.to_dict()['__class__'] + '.' + obj.id
+        return _key
