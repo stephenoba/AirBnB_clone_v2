0558b0990bf17e4e63a401ceac96d9fd2b8a2716
Stephen Oba <obastepheno@gmail.com>
2022-12-07 16:51:48 +0100

    Update SQL script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,13 +7,11 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
     IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
+    IDENTIFIED WITH mysql_native_password BY "hbnb_dev_pwd";
 GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+   ON hbnb_dev_db.*
+   TO 'hbnb_dev'@'localhost';
 GRANT SELECT
    ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+   TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
